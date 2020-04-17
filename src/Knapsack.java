1ec874d63f8cfe2f70fac9ee0b98c18d01c097cb
Dylan Fouche <dylanfouche@gmail.com>
2020-04-17 16:45:20 +0200

    Implemented knapsack class

---
+++ src/Knapsack.java
@@ -0,0 +1,55 @@
+/*
+  D FOUCHE
+  UCT CS HONS
+  FCHDYL001
+*/
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Knapsack
+{
+  ArrayList<knapsackItem> items = new ArrayList<knapsackItem>();
+
+  public class knapsackItem
+  {
+    public int weight;
+    public int value;
+
+    public knapsackItem(int weight, int value){
+      this.weight=weight;
+      this.value=value;
+    }
+  }
+
+  public Knapsack(){
+    try{
+      File file = new File("data/knapsack_instance.csv");
+      Scanner sc = new Scanner(file);
+      String header = sc.nextLine();
+      while (sc.hasNextLine()){
+        String[] line = sc.nextLine().split(";");
+        System.out.println(line.length);
+        knapsackItem item = new knapsackItem(Integer.parseInt(line[1]),Integer.parseInt(line[2]));
+        items.add(item);
+      }
+    }
+    catch(IOException e){
+      System.out.println("Error reading in knapsack contents.");
+      System.exit(0);
+    }
+  }
+
+  @Override
+  public String toString(){
+    String s = "";
+    s += "Item" + "\t" + "Weight" + "\t" + "Value" + "\n";
+    int i=0;
+    for(knapsackItem item : items){
+      s += (++i) + "\t" + item.weight + "\t" + item.value + "\n";
+    }
+    return s;
+  }
+}
---
+++ src/solver.java
@@ -0,0 +1,12 @@
+/*
+  D FOUCHE
+  UCT CS HONS
+  FCHDYL001
+*/
+
+public interface solver
+{
+  public void solve();
+  public void tick();
+  public String toString();
+}
