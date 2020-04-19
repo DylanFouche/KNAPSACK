bf25a09759b164678c56ac1b7fef93858389861d
Dylan Fouche <dylanfouche@gmail.com>
2020-04-19 13:45:40 +0200

    Framework for SAsolver and PSOsolver

---
+++ src/PSOSolver.java
@@ -0,0 +1,21 @@
+/*
+  D FOUCHE
+  UCT CS HONS
+  FCHDYL001
+*/
+
+public class PSOSolver extends Solver
+{
+  public GASolver(Instance instance)
+  {
+    //TODO implement
+  }
+
+  public void solve(){
+    //TODO implement
+  }
+
+  public void tick(){
+    //TODO implement
+  }
+}
