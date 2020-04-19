5cbe0bd009873b5386f157be9084bfbfb512a88e
Dylan Fouche <dylanfouche@gmail.com>
2020-04-19 16:31:08 +0200

    Batch testing shell script for ga with error logging

---
+++ ga_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_01.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_02.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_03.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_04.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_05.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_06.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_07.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_08.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_09.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_10.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_11.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_12.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_13.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_14.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_15.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_16.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_17.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_18.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_19.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_20.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_21.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_22.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_23.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_24.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_25.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_26.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_27.json 2>&1 | tee -a ga_log.txt
+java -cp bin:include/json-20190722.jar driver -configuration ga_default_28.json 2>&1 | tee -a ga_log.txt
+
+
